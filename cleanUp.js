06afc1718d5179e9d55066183ef14754f5484229
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2022-09-22 09:25:39 -0500

    Fix syntax

--- cleanUp.js
+++ cleanUp.js
@@ -3,7 +3,7 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
@@ -17,9 +17,9 @@ nameQuestion()
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third;
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
@@ -37,7 +37,7 @@ makeFreshPesto();
 
 
 //  EX 4:
-function average(num1, num2) 
+function average(num1, num2)
   {
 var sum = num1 + num2;
     var avg = sum / 2;
